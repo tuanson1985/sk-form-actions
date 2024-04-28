4b76c4b2f0962cd2a67a8e3d09372a67a335fe1c
tuansonhq <105621622+tuanson1985@users.noreply.github.com>
2024-04-29 01:51:38 +0700

    Form Action Validation

--- src/routes/auth/+page.server.js
+++ src/routes/auth/+page.server.js
@@ -1,12 +1,15 @@
+import { fail } from '@sveltejs/kit';
+
 export const actions = {
 	login: async ({ request, cookies }) => {
 		const data = await request.formData();
 		const username = data.get('username');
 		const password = data.get('password');
 		if (!username || !password) {
-			return {
+			return fail(400, {
+				username,
 				message: 'Missing username or password'
-			};
+			});
 		}
 		cookies.set('username', username, { path: '/' });
 		return { message: 'Logged in' };
@@ -16,9 +19,10 @@ export const actions = {
 		const username = data.get('username');
 		const password = data.get('password');
 		if (!username || !password) {
-			return {
+			return fail(400, {
+				username,
 				message: 'Missing username or password'
-			};
+			});
 		}
 		cookies.set('username', username, { path: '/' });
 		return { message: 'Successfully registered' };
--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -4,7 +4,7 @@
 
 <form method="post" action="?/login">
 	<p>{form?.message || ''}</p>
-	<input type="text" name="username" placeholder="Username" />
+	<input type="text" name="username" placeholder="Username" value={form?.username ?? ''} />
 	<input type="text" name="password" placeholder="Password" />
 	<button type="submit">Login</button>
 	<button formaction="?/register">Register</button>
