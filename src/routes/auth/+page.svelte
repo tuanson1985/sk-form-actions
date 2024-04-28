44653d0970de859b9612c8f1c4799c4be4fef182
tuansonhq <105621622+tuanson1985@users.noreply.github.com>
2024-04-29 02:07:49 +0700

    Progressive Enhancement

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -1,12 +1,19 @@
 <script>
 	import { page } from '$app/stores';
+	import { enhance } from '$app/forms';
 	export let form;
 	const redirectTo = $page.url.searchParams.get('redirectTo') || '/';
 </script>
 
 <form method="post" action="?/login&redirectTo={redirectTo}">
 	<p>{form?.message || ''}</p>
-	<input type="text" name="username" placeholder="Username" value={form?.username ?? ''} />
+	<input
+		type="text"
+		name="username"
+		placeholder="Username"
+		value={form?.username ?? ''}
+		use:enhance
+	/>
 	<input type="text" name="password" placeholder="Password" />
 	<button type="submit">Login</button>
 	<button formaction="?/register&redirectTo={redirectTo}">Register</button>
