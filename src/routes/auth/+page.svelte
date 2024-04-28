5c6c5f31fa42b7d375fef6beb11f4bb3a2b97ed7
tuansonhq <105621622+tuanson1985@users.noreply.github.com>
2024-04-29 01:28:49 +0700

    Form Actions

---
+++ src/routes/auth/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+	let username = '';
+	let password = '';
+	let message = '';
+	const handleSubmit = async () => {
+		message = '';
+		const response = await fetch('/auth', {
+			method: 'POST',
+			body: JSON.stringify({
+				username,
+				password
+			})
+		});
+
+		const data = await response.json();
+		message = data.message;
+	};
+</script>
+
+<form on:submit|preventDefault={handleSubmit}>
+	<p>{message}</p>
+	<input type="text" name="username" placeholder="Username" bind:value={username} />
+	<input type="text" name="password" placeholder="Password" bind:value={password} />
+	<button type="submit">Login</button>
+</form>
